3a7a8e2765925be6056057ed40f267adb1dfba3f
Maelton Lima <mls54@aluno.ifal.edu.br>
2023-01-08 11:35:36 -0300

    Primeiras definições da aplicação

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainInterface(Widget):
+    pass
+
+class MyFirstApp(App):
+    pass
+
+if __name__ == '__main__':
+    MyFirstApp().run()
