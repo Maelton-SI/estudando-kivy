388591318671b1b69c6521bf13aebf96961dd564
Maelton Lima <mls54@aluno.ifal.edu.br>
2023-01-17 16:22:17 -0300

    Propriedades Button.on_press e Interface.root

--- main.py
+++ main.py
@@ -7,129 +7,26 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 
-class MainInterface(Widget):
-    pass
 
-class SecondInterface(Widget):
-    pass
+class MainInterface(GridLayout):
+    #Tendo em vista que as configurações gráficas foram definidas no arquivo (.kv) não se faz necessária a definição do construtor da classe.
+
+    possibilities = [["Modify label", "Modify me!"], ["Unmodify label", "Umodify me!"]]
+
+    #Criando instâncias de uma propriedade de texto
+    button_text = StringProperty(possibilities[0][0])
+    label_text = StringProperty(possibilities[0][1])
+    
+    def on_click(self):
+        self.possibilities = self.possibilities[::-1]
+        
+        self.button_text = self.possibilities[0][0]
+        self.label_text = self.possibilities[0][1]
 
 class MyFirstApp(App):
     pass
-
-class ThirdInterface(BoxLayout):
-
-    #Definições do construtor de interface pelo arquivo (.py)
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        #Orientação dos objetos dentro da interface.
-        self.orientation = "vertical"
-
-        #Definindo elementos da interface.
-        b1 = Button(
-                text="One",
-                color=(1,0,0,1)
-            )
-        
-        b2 = Button(
-                text="Two",
-                color=(0,1,0,1)
-            )
-        
-        b3 = Button(
-                text="Three",
-                color=(0,0,1,1)
-            )
-
-        #Adicionando elementos à interface.
-        self.add_widget(b1)
-        self.add_widget(b2)
-        self.add_widget(b3)
-
-class FourthInterface(BoxLayout):
-    pass
-
-class FifthInterface(BoxLayout):
-    pass
-
-class SixthInterface(BoxLayout):
-    pass
-
-class SeventhInterface(BoxLayout):
-    pass
-
-class EigthInterface(BoxLayout):
-    pass
-
-class AnchorLayoutExample(AnchorLayout):
-    pass
-
-class TenthInterface(AnchorLayout):
-    pass
-
-'''
-#Essa classe foi definida a partir do arquivo (.kv)
-class GridLayoutExample(GridLayout):
-    pass
-'''
-
-class StackLayoutExample(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        #Valores possíveis para a propriedade orientation (forma com que os elementos são inseridos e exibidos na interface) em um StackLayout:
-        #"lr-tb" (left-right, top-bottom)
-        #"rl-tb" (right-left, top-bottom)
-        #"lr-bt" (left-right, bottom-top)
-        #"rl-bt" (left-right, bottom-top)
-        self.orientation = "lr-tb"
-        
-        #A propriedade padding determina o espaçamento interno da interface em relação a seus elementos.
-        #padding: width, height
-        self.padding = dp(20), dp(100)
-
-        self.spacing = 5
-
-        for button in range(1, 82):
-            b = Button(
-                        text = str(button), 
-                        size_hint = (.1, .1)
-                    )
-
-            self.add_widget(b)
-        
-        for letra in 'ABCDEFG':
-            b = Button(
-                        text = str(letra),
-                        size_hint = (None, None),
-                        size = (dp(50), dp(50))
-            )
-
-            self.add_widget(b)
-
-class TwelvethInterface(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        b = Label(
-                    text = "Serei o primeiro a ser inserido na tela, antes dos elementos definidos no arquivo (.kv)."
-                )
-        
-        self.add_widget(b)
-
-class ThirteenthInterface(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        for button in range(1, 82):
-            b = Button(
-                        text = str(button),
-                        size_hint = (None, None),
-                        size = (dp(100), dp(100))
-                )
-            
-            self.add_widget(b)
 
 if __name__ == '__main__':
     MyFirstApp().run()
