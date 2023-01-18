01df6d7ac4125ddc2d292b9a92d9e3d5d4586e07
Maelton Lima <mls54@aluno.ifal.edu.br>
2023-01-18 17:40:48 -0300

    Interface da calculadora pré-finalizada

--- main.py
+++ main.py
@@ -1,72 +1,41 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.stacklayout import StackLayout
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.metrics import dp
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
 from kivy.properties import StringProperty
-from kivy.properties import BooleanProperty
 
-class MainInterface(GridLayout):
-    count = 0
-    soma_button_disabled = BooleanProperty(True)
-    sub_button_disabled = BooleanProperty(True)
+class MainInterface(BoxLayout):
+    inputed_data = StringProperty("0")
 
-    unidades = StringProperty(str(count))
-    status_botao = StringProperty("Off")
-    
-    texto_instrutivo = StringProperty("Ligue a calculadora para poder calcular!")
-    texto_explicativo = StringProperty()
+class OutputArea(BoxLayout):
+    pass
 
-    def soma_unidade(self):
-        if not self.soma_button_disabled:
-            self.count += 1
-            self.unidades = str(self.count)
-        
-        if self.count == 10:
-            self.soma_button_disabled = True
-            self.texto_explicativo = "Esta calculadora não admite números maiores que 10 unidades."
-        else:
-            self.sub_button_disabled = False
-            self.texto_explicativo = ""
-    
-    def subtrai_unidade(self):
-        if not self.sub_button_disabled:
-            self.count -= 1
-            self.unidades = str(self.count)
+class InputArea(BoxLayout):
+    pass
 
-        if self.count == 0:
-            self.sub_button_disabled = True
-            self.texto_explicativo = "Esta calculadora não admite números negativos."
+class Top(BoxLayout):
+    pass
 
-        else:
-            self.soma_button_disabled = False
-            self.texto_explicativo = ""
-    
-    def liga_desliga(self, botao_toggle):
-        if botao_toggle.state == "normal":
-            self.soma_button_disabled = True
-            self.sub_button_disabled = True
+class Middle(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-            self.count = 0
-            self.unidades = str(self.count)
-            
-            self.status_botao = "Off"
-            self.texto_instrutivo = "Ligue a calculadora para poder calcular!"
-            self.texto_explicativo = ""
+        for number in range(1,10):
+            number_button = Button(
+                                    text = str(number)
+                            )
+            self.add_widget(number_button)
 
-        elif botao_toggle.state == "down":
-            self.soma_button_disabled = False
-            
-            self.status_botao = "On"
-            self.texto_instrutivo = "Calculadora somatória ligada!"
-            self.texto_explicativo = "Esta calculadora não admite números negativos."
+class Right(BoxLayout):
+    pass
 
-class MyFirstApp(App):
+class Bottom():
+    pass
+
+class CalcApp(App):
     pass
 
 if __name__ == '__main__':
-    MyFirstApp().run()
+    CalcApp().run()
